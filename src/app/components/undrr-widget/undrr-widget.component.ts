c68690243284c44575e2b5b0f78c060d044b31c4
Ken Hawkins <khawkins98@gmail.com>
2025-03-11 10:25:54 +0000

    Make component more reusable

--- src/app/components/undrr-widget/undrr-widget.component.ts
+++ src/app/components/undrr-widget/undrr-widget.component.ts
@@ -1,19 +1,31 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, Input } from '@angular/core';
 import { ScriptLoaderService } from '../../services/script-loader.service';
 
 @Component({
   standalone: true,
   selector: 'app-undrr-widget',
-  template: `<section class="pw-widget-sfm-page">Loading...</section>`,
-  styles: [`
-    .pw-widget-sfm-page {
-      min-height: 100px;
-      border: 3px solid red;
-    }
-  `],
-  providers: [ScriptLoaderService]
+  template: `<section
+    class="pw-widget"
+    [class]="'undrr-widget pw-widget-' + suffixID"
+  >
+    Loading... {{ suffixID }}
+  </section>`,
+  styles: [
+    `
+      .undrr-widget {
+        min-height: 100px;
+        border: 2px solid red;
+        padding: 2rem;
+        margin: 2rem;
+      }
+    `,
+  ],
+  providers: [ScriptLoaderService],
 })
 export class UndrrWidgetComponent implements AfterViewInit {
+  @Input() pageid!: string; // ✅ Accept pageid as input
+  @Input() suffixID!: string; // ✅ Accept suffixID as input
+
   private scriptUrl = 'https://publish.preventionweb.net/widget.js?rand=3d797b';
   private nonce = '20240509121740'; // If required by CSP
 
@@ -25,17 +37,17 @@ export class UndrrWidgetComponent implements AfterViewInit {
 
       const pwWidget = (window as any).PW_Widget;
 
-      if (pwWidget) {
+      if (pwWidget && typeof pwWidget.initialize === 'function') {
         pwWidget.initialize({
           contenttype: 'landingpage',
-          pageid: '92409',
+          pageid: this.pageid,
           includemetatags: false,
           includecss: true,
-          suffixID: 'sfm-page',
-          activedomain: 'www.preventionweb.net'
+          suffixID: this.suffixID,
+          activedomain: 'www.preventionweb.net',
         });
       } else {
-        console.error('PW_Widget is not available');
+        console.error('PW_Widget is not available or not a function');
       }
     } catch (error) {
       console.error(error);
